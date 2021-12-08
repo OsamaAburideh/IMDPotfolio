e0c18bed9095a30c6e7f549b5fdf11f7d9c64e4a
Osama Aburideh <x00154068@mytudublin.ie>
2021-12-08 04:33:58 +0000

    First commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+                            strings: ["Software Developer", "Developer", "Blogger", "Designer", "Freelancer"],
+                        
+                            typeSpeed: 10,
+                            backSpeed: 10,
+                            loop: true
+                            
+                        
+                            
+                    });
+                });
